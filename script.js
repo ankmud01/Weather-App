359eead1e0f5c8fb62b4b9ad70624841f4d51fa8
Rebel Soul <mudvari.ankit@gmail.com>
2020-04-09 23:24:03 -0400

    Added the 5 day forecast and clicking on  past search to get weather functionality...

--- script.js
+++ script.js
@@ -1,55 +1,64 @@
-//declaring some variables
+//declaring variables
 var searchSection = $("form");
 var searchBox = $("#search")
 var cityList = $("#search-hist");
 
-//getting current day
+//getting current day using moment js
 const currentDate = moment();
 console.log("Today's date is: " + currentDate.format('MM/DD/YYYY'));
 
-//function to convert date
-function dateConvert(epoch){
-    var date = moment().format('MM/DD/YYYY');
+//function to convert epoch from response to redable date using Date().get..()
+function dateConvert(epoch) {
+    if (epoch < 10000000000)
+        epoch *= 1000;  // convert to milliseconds (Epoch is usually expressed in seconds, but Javascript uses Milliseconds)
+    var epoch = epoch + (new Date().getDate());
+    return moment(epoch).format('MM/DD/YYYY');
+
 }
 
-//Function to add searched city names to the past cities section
-function addCityList(cityValue){
-    var currLocation = $("<div>");
-    currLocation.addClass("btn waves-effect pastcity");
-    currLocation.attr("data-name", cityValue)
-    currLocation.text(cityValue.trim().toUpperCase())
-    cityList.append(currLocation);
-} 
+//Function to add searched city names to the past search section
+function addCityList(cityValue) {
+    var currLocation = $("<div>");                          //creating a div
+    currLocation.addClass("btn waves-effect pastcity");     //adding class to the new div
+    currLocation.attr("data-name", cityValue)               //adding attribute
+    currLocation.text(cityValue.trim().toUpperCase())       //adding cityValue to div and changing it to uppercase
+    cityList.append(currLocation);                          //appending the div to past search section
+}
 
-//Function to create div to put information for hourly forecast
-function hourlyForecast(list){
-    for (var i=0; i < list.length; i++){
-        var dailydiv = $("<div>").attr("class","col s2");
-        var dailycard = $("<div>").attr("class", "card dailyforecast");
-        var dailycontent = $("<h6>").attr("class", "content");
+//Function to create div to put information for hourly forecast section
+function hourlyForecast(list) {
+    for (var i = 0; i < list.length; i++) {
+//Creating div element and a header tag to place the lines which has the list of values
+        var dailydiv = $("<div>").attr("class", "col s2");                  
+        var dailycard = $("<div>").attr("class", "card dailyforecast");     
+        var dailycontent = $("<h6>").attr("class", "content");              
         dailycontent.text(list[i].date)
-        console.log(dailycontent);
 
-        var dailyul = $("<ul>");
-        var iconli = $("<li>")
-        var iconimage = $("<img>");
-        iconimage.attr("src", list[i].icon);
-        var weatherli = $("<li>");
-        weatherli.text(list[i].weather);
-        var templi = $("<li>");
-        templi.text=('Temperature: ' + list[i].temperature + " °F");
+//Creating lines dynamically to place the values from the list which is input to this function
+        var dailyul = $("<ul>");                                            
+        var iconli = $("<li>")                                              
+        var iconimage = $("<img>");                                         
+        iconimage.attr("src", list[i].icon);                                
+        var weatherli = $("<li>");                                          
+        weatherli.text(list[i].weather);                                    
+        var templi = $("<li>");                                             
+        templi.text("Temperature: " + list[i].temperature + " °F");        
         var humidli = $("<li>");
-        humidli.text=('Humidity: ' + list[i].humidity + "%");
+        humidli.text("Humidity: " + list[i].humidity + "%");
 
+//Appending the created li elements to the ul(dailyul) elements     
         iconli.append(iconimage);
         dailyul.append(iconli);
         dailyul.append(weatherli);
         dailyul.append(templi);
         dailyul.append(humidli);
 
+//Appending the ul from above to the parent div and it's associated parent div
         dailycontent.append(dailyul);
         dailycard.append(dailycontent);
         dailydiv.append(dailycard);
+
+//Appending all the dynamically created div to the static div on the html page
         $("#forecast").append(dailydiv);
     }
 
@@ -57,13 +66,13 @@ function hourlyForecast(list){
 
 
 //Function to get the weather condition for the day and forecast for 5 days
-function weatherForecast(city){
+function weatherForecast(city) {
     //This api call is to get the current weather condition for a city
     var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=dbc47b8d78e589660c7834e13c830d1b";
     $.ajax({
         url: currentWeatherURL,
         method: "GET"
-    }).then(function(currWeathresponse){
+    }).then(function (currWeathresponse) {
         console.log(currWeathresponse);
         $("#weatherIcon").attr("src", "http://openweathermap.org/img/wn/" + currWeathresponse.weather[0].icon + "@2x.png");
         $("#cityName").text(currWeathresponse.name).append(" (" + currentDate.format('MM/DD/YYYY') + ")");
@@ -75,27 +84,28 @@ function weatherForecast(city){
         console.log(longitude)
         var latitude = currWeathresponse.coord.lat;
         console.log(latitude)
-
+    //This api call utilizes the longitude and latitude from above call and finds the uv index. The for loop is for the color based on uv index value
         var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=dbc47b8d78e589660c7834e13c830d1b&lat=" + latitude + "&lon=" + longitude;
         $.ajax({
             url: uvURL,
             method: "GET"
-        }).then(function(uvresponse){
+        }).then(function (uvresponse) {
             console.log(uvresponse)
             var uvValue = parseFloat(uvresponse.value);
             let color = '';
-            if(uvValue < 3){
+            if (uvValue < 3) {
                 color = 'green';
-            }else if(uvValue < 6){
-                color='yellow';
-            }else if(uvValue < 8){
-                color='orange'
-            }else if(uvValue < 11){
-                color='red'
-            }else{
-                color='violet'
+            } else if (uvValue < 6) {
+                color = 'yellow';
+            } else if (uvValue < 8) {
+                color = 'orange'
+            } else if (uvValue < 11) {
+                color = 'red'
+            } else {
+                color = 'violet'
             }
-
+        
+        //adding a span element to the variable which holds uv value so that i can color the value
             var uvColor = "UV Index: " + `<span style="background-color:${color}; padding: 0 7px 0 7px;">${uvresponse.value}</span>`;
             console.log(uvColor);
 
@@ -106,36 +116,49 @@ function weatherForecast(city){
 
     var weatherlistArr = [];
     var weatherlistObj = {};
-    var hourlyURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&cnt=6&units=imperial&appid=dbc47b8d78e589660c7834e13c830d1b";
+    var hourlyURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=dbc47b8d78e589660c7834e13c830d1b";
     $.ajax({
         url: hourlyURL,
         method: "GET"
-    }).then(function(hourlyWeathresponse){
+    }).then(function (hourlyWeathresponse) {
         console.log(hourlyWeathresponse);
-        for(var i=1; i < hourlyWeathresponse.list.length; i++){
+        for (var i = 3; i < hourlyWeathresponse.list.length; i += 8) {
             var weatherlist = hourlyWeathresponse.list[i];
-            console.log (weatherlist);
+            console.log(weatherlist);
             weatherlistObj = {
                 date: dateConvert(weatherlist.dt),
                 weather: weatherlist.weather[0].description,
-                icon: "http://openweathermap.org/img/wn/" + weatherlist.weather[0].icon + "@2x.png",
+                icon: "http://openweathermap.org/img/w/" + weatherlist.weather[0].icon + ".png",
                 temperature: weatherlist.main.temp,
                 humidity: weatherlist.main.humidity
             };
+            console.log(weatherlistObj);
             weatherlistArr.push(weatherlistObj);
-            console.log(weatherlistArr);
         }
 
         hourlyForecast(weatherlistArr);
+
     })
 
 }
 
-//Adding eventlistener to get the city name text
-$("#getWeather").click(function(e){
+//Adding eventlistener to get the city name text and load all the weather for the city
+$("#getWeather").click(function (e) {
     e.preventDefault();
     weatherForecast(searchBox.val().trim());
     addCityList(searchBox.val().trim());
-    searchBox.val('');  //This will clear out the searchbox after submiting a city name
-    
+    searchBox.val('');          //This will clear out the searchbox after submiting a city name
+    $("#forecast").text('');    //This will stop appending next city's 5 day forecast to current city
+
 });
+
+//Adding eventlistener to get the weather for the city when one of the past searched city is clicked
+$("#search-hist").click(function(event){
+    event.preventDefault();
+    var pastCityName = $(event.target)[0]
+    if (pastCityName.className === "btn waves-effect pastcity"){
+        console.log(pastCityName.innerText);
+        weatherForecast(pastCityName.innerText.trim().toLowerCase());
+        $("#forecast").text('');
+    }
+})
